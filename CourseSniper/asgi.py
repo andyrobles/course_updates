2102909facdc960cf41f88d5386ddc4ff1cd385c
andrewrobles <andrewrobles@berkeley.edu>
2020-03-15 15:37:12 -0700

    Initial commit

---
+++ CourseSniper/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CourseSniper project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CourseSniper.settings')
+
+application = get_asgi_application()

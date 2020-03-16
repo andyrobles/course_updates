986265cf35371cbdf37c5d42020d4bc01097e571
andrewrobles <andrewrobles@berkeley.edu>
2020-03-15 20:24:18 -0700

    Create form template

--- spotter/views.py
+++ spotter/views.py
@@ -5,5 +5,9 @@ def index(request):
 	return render(request, 'spotter/index.html')
 
 def create_account(request):
-	return render(request, 'spotter/form.html', {'form': AccountForm()})
+	return render(request, 'spotter/form.html', {
+		'form': AccountForm(),
+		'icon': 'user-plus',
+		'modal_title': 'Create Account'
+	})
 		

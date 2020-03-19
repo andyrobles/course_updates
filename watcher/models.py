cd5f109a59c23ba110ccccf55ef63201d0ccb1b0
arobles <arobles@gblsys.com>
2020-03-19 15:56:45 -0700

    Define a course model

--- watcher/models.py
+++ watcher/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    crn = models.IntegerField()
+    title = models.CharField(max_length=200)
+    instructor = models.CharField(max_length=200)
+    meeting_time = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    waitlist_availability = models.CharField(max_length=200)

98562218ff598fe652b0b4bca8ef7ac6dc2c0068
andrewrobles <andrewrobles@berkeley.edu>
2020-03-25 12:41:38 -0700

    Refactor CourseSnapshot

--- courses/utilities/course.py
+++ courses/utilities/course.py
@@ -6,15 +6,18 @@ class CourseSnapshot:
         self._crn = crn
         self.course_search_results_soup = self.get_course_search_results_soup()
         self.course_details_soup = self.get_course_details_soup()
-        self._course_attributes = {
-            'crn': self._get_crn,
-            'title': self._get_title,
-            'instructor': self._get_instructor,
-            'meeting_time': self._get_meeting_time,
-            'location': self._get_location,
-            'status': self._get_status,
-            'seating_availability': self._get_seating_availability,
-            'waitlist_availability': self._get_waitlist_availability
+        self._course_attributes = self._parse_course_attributes() 
+
+    def _parse_course_attributes(self):
+        return {
+            'crn': str(self._crn), 
+            'title': self.course_search_results_soup.findAll("td", {"class": "crn_header"})[0].get_text().rstrip(),
+            'instructor': self._parse_course_search_results(2, 3, 9) if self._is_distance_education_class() else self._parse_course_search_results(2, 3, 16),
+            'meeting_time': 'Distance Education Class' if self._is_distance_education_class() else '{} {}'.format(self._weekdays, self._time_span),
+            'location': self._parse_course_search_results(2, 3, 5).strip() if self._is_distance_education_class() else self._parse_course_search_results(2, 3, 12).strip(),
+            'status': self._parse_course_search_results(2, 3, 0),
+            'seating_availability': '{} out of {} spots open'.format(self._parse_course_details(2, 2, 2), self._parse_course_details(2, 2, 0)),
+            'waitlist_availability': '{} out of {} spots open'.format(self._parse_course_details(2, 2, 5), self._parse_course_details(2, 2, 3)),
         }
 
     @property
@@ -49,33 +52,17 @@ class CourseSnapshot:
     def waitlist_availability(self):
         return self._course_attributes['waitlist_availability']
 
-    @property
-    def _get_crn(self):
-        return str(self._crn)
+    def _parse_scraped_data(self, scraped_data, table_index, tr_index, td_index):
+        return scraped_data.find_all('table')[table_index].find_all('tr')[tr_index].find_all('td')[td_index].get_text()
 
-    @property
-    def _get_title(self):
-        title = self.course_search_results_soup.findAll("td", {"class": "crn_header"})[0].get_text() 
-        return title.rstrip()
+    def _parse_course_search_results(self, table_index, tr_index, td_index):
+        return self._parse_scraped_data(self.course_search_results_soup, table_index, tr_index, td_index)
 
-    @property
-    def _get_instructor(self):
-        if self._is_distance_education_class():
-            return self._parse_scraped_data(2, 3, 9)
-        else:
-            return self._parse_scraped_data(2, 3, 16)
-
-    def _parse_scraped_data(self, table_index, tr_index, td_index):
-        return self.course_search_results_soup.find_all('table')[table_index].find_all('tr')[tr_index].find_all('td')[td_index].get_text()
-
-    @property
-    def _get_meeting_time(self):
-        if self._is_distance_education_class():
-            return 'Distance Education Class'
-        return '{} {}'.format(self._weekdays, self._time_span)
+    def _parse_course_details(self, table_index, tr_index, td_index):
+        return self._parse_scraped_data(self.course_details_soup, table_index, tr_index, td_index)
 
     def _weekday(self, index):
-        weekday = self._parse_scraped_data(2, 3, 4 + index)
+        weekday = self._parse_course_search_results(2, 3, 4 + index)
         return weekday
 
     def _is_distance_education_class(self):
@@ -99,52 +86,11 @@ class CourseSnapshot:
 
     @property
     def _time_span(self):
-        time = self._parse_scraped_data(2, 3, 11)
+        time = self._parse_course_search_results(2, 3, 11)
         return time
 
-    @property
-    def _get_location(self):
-        if self._is_distance_education_class():
-            location = self._parse_scraped_data(2, 3, 5)
-        else:
-            location = self._parse_scraped_data(2, 3, 12)
-        return location.strip()
-
-    @property
-    def _get_status(self):
-        status = self._parse_scraped_data(2, 3, 0)
-        return status
-
-    @property
-    def _get_seating_availability(self):
-        return '{} out of {} spots open'.format(self._seating_spots_left, self._total_seating_spots)
-
-    @property
-    def _seating_spots_left(self):
-        spots_left = self.course_details_soup.find_all('table')[2].find_all('tr')[2].find_all('td')[2].get_text()
-        return spots_left
-
-    @property
-    def _total_seating_spots(self):
-        total_spots = self.course_details_soup.find_all('table')[2].find_all('tr')[2].find_all('td')[0].get_text()
-        return total_spots
-
-    @property
-    def _get_waitlist_availability(self):
-        return '{} out of {} spots open'.format(self.waitlist_spots_left, self._total_waitlist_spots)
-
-    @property
-    def waitlist_spots_left(self):
-        spots_left = self.course_details_soup.find_all('table')[2].find_all('tr')[2].find_all('td')[5].get_text()
-        return spots_left
-
-    @property
-    def _total_waitlist_spots(self):
-        total_spots = self.course_details_soup.find_all('table')[2].find_all('tr')[2].find_all('td')[3].get_text()
-        return total_spots
-
     def get_course_search_results_html(self):
-        crn = self._get_crn
+        crn = str(self._crn) 
         url = 'https://ssb.vcccd.edu/prod/pw_pub_sched.p_listthislist'
         url_values = 'TERM=202007&TERM_DESC=Fall+2020&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_camp=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_subj=%25&sel_crse=&sel_crn=' + crn + '&sel_title=&sel_ptrm=%25&sel_camp=%25&sel_instr=%25&begin_hh=5&begin_mi=0&begin_ap=a&end_hh=11&end_mi=0&end_ap=p&sel_new1=&aa=N&bb=N&dd=N&ee=N&ff=N&ztc=N'
         full_url = url + '?' + url_values
@@ -157,7 +103,7 @@ class CourseSnapshot:
         return course_search_results_soup
 
     def get_course_details_html(self):
-        url = 'https://ssb.vcccd.edu/prod/pw_pub_sched.p_course_popup?vsub=CS&vcrse=M10P&vterm=202007&vcrn={}'.format(self._get_crn)
+        url = 'https://ssb.vcccd.edu/prod/pw_pub_sched.p_course_popup?vsub=CS&vcrse=M10P&vterm=202007&vcrn={}'.format(str(self._crn))
         html_doc = urllib.request.urlopen(url).read()
         return html_doc
 

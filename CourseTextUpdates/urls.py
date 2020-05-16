9a9bd7cfdb540885bafd7e45f5414ace5b43e927
Andy Robles <andyrobles805@gmail.com>
2020-05-15 23:58:57 -0700

    Redirect base url to courses app.

--- CourseTextUpdates/urls.py
+++ CourseTextUpdates/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='account')),
-    path('courses/', include(('courses.urls', 'courses'), namespace='courses')),
+    path('', include(('courses.urls', 'courses'), namespace='courses')),
     path('admin/', admin.site.urls),
 ]
